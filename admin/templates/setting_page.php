8a6acdb98125f35949c92158f776f13ed56c4ad1
manno-k <art.kuromi@gmail.com>
2017-10-06 20:47:30 +0900

    ・テスト

--- admin/templates/setting_page.php
+++ admin/templates/setting_page.php
@@ -1,71 +1,70 @@
 <?php
-function get_acf_option($val)
-{
-    if (!empty($_POST[$val])) {
-        return $name = htmlspecialchars($_POST[$val]);
-        return update_option($val, $name);
-    }
-    if (!get_option($val)) {
-        return add_option($val, '');
-    } else {
-        return $name = get_option($val);
-    }
-}
-$key = get_acf_option('acf_field_key');
-$name1 = get_acf_option('name1');
-$name2 = get_acf_option('name2');
-$name3 = get_acf_option('name3');
-$name4 = get_acf_option('name4');
-  //
-  // if (!empty($_POST['acf_field_key'])) {
-  //     $key = htmlspecialchars($_POST['acf_field_key']);
-  //     update_option('acf_field_key', $key);
-  // }
-  // if (!get_option('acf_field_key')) {
-  //     add_option('acf_field_key', '');
-  // } else {
-  //     $key = get_option('acf_field_key');
-  // }
-  //
-  // if (!empty($_POST['name1'])) {
-  //     $name1 = htmlspecialchars($_POST['name1']);
-  //     update_option('name1', $name1);
-  // }
-  // if (!get_option('name1')) {
-  //     add_option('name1', '');
-  // } else {
-  //     $name1 = get_option('name1');
-  // }
-  //
-  // if (!empty($_POST['name2'])) {
-  //     $name2 = htmlspecialchars($_POST['name2']);
-  //     update_option('name2', $name2);
-  // }
-  // if (!get_option('name2')) {
-  //     add_option('name2', '');
-  // } else {
-  //     $name2 = get_option('name2');
-  // }
-  //
-  // if (!empty($_POST['name3'])) {
-  //     $name3 = htmlspecialchars($_POST['name3']);
-  //     update_option('name3', $name3);
-  // }
-  // if (!get_option('name3')) {
-  //     add_option('name3', '');
-  // } else {
-  //     $name3 = get_option('name3');
-  // }
-  //
-  // if (!empty($_POST['name4'])) {
-  //     $name4 = htmlspecialchars($_POST['name4']);
-  //     update_option('name4', $name4);
-  // }
-  // if (!get_option('name4')) {
-  //     add_option('name4', '');
-  // } else {
-  //     $name4 = get_option('name4');
-  // }
+// function get_acf_option($val){
+//     if (!empty($_POST[$val])) {
+//     return $name = htmlspecialchars($_POST[$val]);
+//     return update_option($val, $name);
+//   }
+//   if (!get_option($val)) {
+//       return add_option($val, '');
+//   } else {
+//       return $name = get_option($val);
+//   }
+// }
+// $key = get_acf_option('acf_field_key');
+// $name1 = get_acf_option('name1');
+// $name2 = get_acf_option('name2');
+// $name3 = get_acf_option('name3');
+// $name4 = get_acf_option('name4');
+
+  if (!empty($_POST['acf_field_key'])) {
+      $key = htmlspecialchars($_POST['acf_field_key']);
+      update_option('acf_field_key', $key);
+  }
+  if (!get_option('acf_field_key')) {
+      add_option('acf_field_key', '');
+  } else {
+      $key = get_option('acf_field_key');
+  }
+
+  if (!empty($_POST['name1'])) {
+      $name1 = htmlspecialchars($_POST['name1']);
+      update_option('name1', $name1);
+  }
+  if (!get_option('name1')) {
+      add_option('name1', '');
+  } else {
+      $name1 = get_option('name1');
+  }
+
+  if (!empty($_POST['name2'])) {
+      $name2 = htmlspecialchars($_POST['name2']);
+      update_option('name2', $name2);
+  }
+  if (!get_option('name2')) {
+      add_option('name2', '');
+  } else {
+      $name2 = get_option('name2');
+  }
+
+  if (!empty($_POST['name3'])) {
+      $name3 = htmlspecialchars($_POST['name3']);
+      update_option('name3', $name3);
+  }
+  if (!get_option('name3')) {
+      add_option('name3', '');
+  } else {
+      $name3 = get_option('name3');
+  }
+
+  if (!empty($_POST['name4'])) {
+      $name4 = htmlspecialchars($_POST['name4']);
+      update_option('name4', $name4);
+  }
+  if (!get_option('name4')) {
+      add_option('name4', '');
+  } else {
+      $name4 = get_option('name4');
+  }
 ?>
 <div class="wrap">
   <h2>フィールドキー登録画面</h2>
